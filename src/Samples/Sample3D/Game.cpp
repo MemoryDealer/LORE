c44bbdd8e0d4a1b7a0c662af8438e635c2f3ef38
Jordan <dreamwizard@protonmail.com>
2021-06-10 16:51:22 -0500

    Sync: Adding space shader

--- src/Samples/Sample3D/Game.cpp
+++ src/Samples/Sample3D/Game.cpp
@@ -364,9 +364,12 @@ void Game::loadCustomShaders()
 
     uniform mat4 transform;
 
+    out vec4 FragPos;
+
     void main()
     {
       gl_Position = transform * vec4(vertex, 1.0);
+      FragPos = transform * vec4(vertex, 1.0);
     }
 
   )" );
@@ -381,12 +384,90 @@ void Game::loadCustomShaders()
 
   src = header;
 
+  //src.append( R"(
+  //  out vec4 pixel;
+
+  //  uniform float time;
+
+  //  void main()
+  //  {
+  //    float g = abs(sin(time));
+  //    float r = abs(cos(time));
+  //    pixel = vec4(r, g, 0.0, 1.0);
+  //  }
+  //)" );
+  
+  // Modified from source: http://glslsandbox.com/e#73375.0
   src.append( R"(
     out vec4 pixel;
 
+    in vec4 FragPos;
+
+    uniform float time;
+    uniform mat4 view;
+
+    #define iterations 15
+    #define formuparam 0.530
+
+    #define volsteps 18
+    #define stepsize 0.120
+
+    #define zoom   1.900
+    #define tile   0.850
+    #define speed  0.2
+
+    #define brightness 0.0015
+    #define darkmatter 0.400
+    #define distfading 0.760
+    #define saturation 0.800
+
     void main()
     {
-      pixel = vec4(0.0, 1.0, 0.0, 1.0);
+      vec2 resolution = vec2(1000.0, 1000.0);
+
+      //get coords and direction
+      vec4 fragCoord = gl_FragCoord;
+	    vec2 uv=fragCoord.xy/resolution.xy-.5;
+	    uv.y*=resolution.y/resolution.x;
+	    vec3 dir=vec3(uv*zoom,1.);
+	
+	    float a2=time*0.0+.5;
+	    float a1=0.0;
+	    mat2 rot1=mat2(cos(a1),sin(a1),-sin(a1),cos(a1));
+	    mat2 rot2=rot1;//mat2(cos(a2),sin(a2),-sin(a2),cos(a2));
+	    dir.xz*=rot1;
+	    dir.xy*=rot2;
+	
+	    //from.x-=time;
+	    vec3 from=vec3(0.,0.,0.);
+	    from+=vec3(.05*time*speed,.05*time*speed,-2.);
+	
+	    from.xz*=rot1;
+	    from.xy*=rot2;
+	
+	    //volumetric rendering
+	    float s=.4,fade=.2;
+	    vec3 v=vec3(0.4);
+	    for (int r=0; r<volsteps; r++) {
+		    vec3 p=from+s*dir*.5;
+		    p = abs(vec3(tile)-mod(p,vec3(tile*2.))); // tiling fold
+		    float pa,a=pa=0.;
+		    for (int i=0; i<iterations; i++) { 
+			    p=abs(p)/dot(p,p)-formuparam; // the magic formula
+			    a+=abs(length(p)-pa); // absolute sum of average change
+			    pa=length(p);
+		    }
+		    float dm=max(0.,darkmatter-a*a*.001); //dark matter
+		    a*=a*a*2.; // add contrast
+		    if (r>3) fade*=1.-dm; // dark matter, don't render near
+		    //v+=vec3(dm,dm*.5,0.);
+		    v+=fade;
+		    v+=vec3(s,s*s,s*s*s*s)*a*brightness*fade; // coloring based on distance
+		    fade*=distfading; // distance fading
+		    s+=stepsize;
+	    }
+	    v=mix(vec3(length(v)),v,saturation); //color adjust
+	    pixel = vec4(v*.01,1.);
     }
   )" );
 
@@ -406,12 +487,19 @@ void Game::loadCustomShaders()
   }
 
   program->addTransformVar( "transform" );
+  program->addUniformVar( "time" );
+  program->addUniformVar( "view" );
 
   {
     auto UniformUpdater = []( const Lore::RenderView& rv,
                               const Lore::GPUProgramPtr program,
                               const Lore::MaterialPtr material,
                               const Lore::RenderQueue::LightData& lights ) {
+      static float time = 0.0f;
+      time += 0.01f;
+      program->setUniformVar( "time", time );
+
+      program->setUniformVar( "view", rv.camera->getViewMatrix() );
     };
 
     auto UniformNodeUpdater = []( const Lore::GPUProgramPtr program,
